b97cb6a7ba17dc4542b981a296681e9c32b3b08e
Jack Grzegorz Rospara <jackoofrost@gmail.com>
2023-05-17 16:10:18 +0200

    6.6. Zadanie: rozbudowa BookLibrary

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,13 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Processor;
+
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        Processor processor = new Processor();                                // [6]
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();  // [7]
+        processor.execute(executeSaySomething);                               // [8]
     }
 }
