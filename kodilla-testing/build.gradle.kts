a9344a161089fbb49c414a634a22cdcb5a21ef76
Ichimaroo <115849557+Ichimaroo@users.noreply.github.com>
2023-05-17 14:33:56 +0200

    Update build.gradle.kts

--- kodilla-testing/build.gradle.kts
+++ kodilla-testing/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation group: 'org.mockito', name: 'mockito-junit-jupiter', version: '5.3.1'
+    testImplementation ("org.mockito:mockito-junit-jupiter:5.3.1")
 }
 
 tasks.test {
