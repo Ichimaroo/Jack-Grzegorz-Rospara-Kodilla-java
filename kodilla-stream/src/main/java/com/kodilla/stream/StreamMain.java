072802de479868ca1562b0862f0c05aae4f1fd99
Jack Grzegorz Rospara <jackoofrost@gmail.com>
2023-05-18 17:36:26 +0200

    7.2 Zadanie: niemutowalny uzytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,40 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+
 
 
 public class StreamMain {
     public static void main(String[] args) {
-        Processor processor = new Processor();                                // [6]
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();  // [7]
-        processor.execute(executeSaySomething);                               // [8]
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        System.out.println(poemBeautifier.beautify("Coś", text -> text.toUpperCase()));
+        System.out.println(poemBeautifier.beautify("Invictus", text -> "ABC" + text + "ABC"));
+        System.out.println(poemBeautifier.beautify("AGDAAA", text -> text.toLowerCase()));
+        System.out.println(poemBeautifier.beautify("Something jest super, Something", text -> text.replace("Something", "Warszawa")));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
